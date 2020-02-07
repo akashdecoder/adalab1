7d1e44045211b604a0822401e523869a58329f0b
AkashDecoder <akash.ranjan1999@gmail.com>
2020-02-07 16:53:11 +0530

    Update Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,41 +1,30 @@
-#include <bits/stdc++.h>
-#include <algorithm>
-using namespace std;
-const int MAX=3;
-void Knapsack(int n,int w[MAX],int pr[MAX],int cap){
-	int table[n][n];
-	int subset[n];
-	for(int i=0;i<=n;i++){
-		for(int j=0;j<=cap;j++){
-			if(i == 0 || j == 0)
-				table[i][j] = 0;
-			else if((w[i-1])<=j)
-				table[i][j] = max(table[i-1][j],table[i-1][j-w[i-1]]+pr[i-1]);
-			else
-				table[i][j] = table[i-1][j];
-		}
-	}
-	int max_profit = table[n][cap];
-	for(int i=0;i<n;i++){
-		subset[i] = 0;
-	}
-	int a=n;
-	int b=cap;
-	while(a >=1 && b>=1){
-		if(table[a][b] != table[a-1][b]){
-			subset[a] = 1;
-			b = b - w[a];
-		}
-		a = a-1;
-	}
-	cout<<"\nMaximum Profit: "<<max_profit<<endl;
-	for(int i=0;i<n;i++){
-		cout<<subset[i]<<"\t";
-	}
-	cout<<endl;
-}
 
-int main(){
+#include <bits/stdc++.h>
+
+using namespace std;
+int max(int a, int b) { 
+	return (a > b)? a : b; 
+} 
+
+
+int knapsack(int n, int wt[], int val[], int W) { 
+	int i, w; 
+	int K[n+1][W+1]; 
+	for (i = 0; i <= n; i++){ 
+	 
+		for (w = 0; w <= W; w++) { 
+			if (i==0 || w==0) 
+				K[i][w] = 0; 
+			else if (wt[i-1] <= w) 
+					K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]); 
+			else
+					K[i][w] = K[i-1][w]; 
+		}
+	} 
+	return K[n][W]; 
+} 
+
+int main() { 
 	int n,c;
 	cin>>n>>c;
 	int w[n],pr[n];
@@ -45,6 +34,6 @@ int main(){
 	for(int i=0;i<n;i++){
 		cin>>pr[i];
 	}
-	Knapsack(n,w,pr,c);
+	cout<<knapsack(n,w,pr,c);
 	return 0;
-}
+} 
